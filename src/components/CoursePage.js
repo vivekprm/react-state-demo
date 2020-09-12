5770739c2e8e064a9c6ecc792695fc37bf477785
vivekprm <vivek.pr.mi@gmail.com>
2020-09-13 00:07:36 +0530

    Converting to functional components.

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,45 +1,42 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursePage extends Component {
-  state = {
-    courses: [],
-  };
+function CoursePage() {
+  const [courses, setCourses] = useState([]);
 
-  componentDidMount() {
-    getCourses().then((courses) =>
-      this.setState({
-        courses: courses,
-      })
-    );
-  }
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>         
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map((course) => {
-              return (
-                <tr>
-                  <td>{course.title}</td>
-                  <td>{course.authorId}</td>
-                  <td>{course.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+  /**
+   * It's important to pass [] array. So that useEffect runs only when it's empty.
+   */
+  useEffect(() => {
+    getCourses().then((_courses) => {
+      setCourses(_courses);
+    });
+  }, []);
+  return (
+    <>
+      <h2>Courses</h2>         
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => {
+            return (
+              <tr>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default CoursePage;
