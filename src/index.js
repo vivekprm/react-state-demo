7579d99f909efb9d385a95dad01a123de03772ed
vivekprm <vivek.pr.mi@gmail.com>
2020-09-13 01:01:09 +0530

    react router configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "react-dom";
 import App from "./components/App";
 
 // First argument is the component we want to render, second argument is target where
 // we want to render.
-render(<App />, document.getElementById("root"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
